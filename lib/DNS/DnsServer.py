53c216b278c8adb9d562238463603533cef4c8de
MaciaKing <maciasalvasalva@gmail.com>
2022-09-28 13:28:24 +0000

    ..

--- lib/DNS/DnsServer.py
+++ lib/DNS/DnsServer.py
@@ -1,46 +1,54 @@
 import dns
 import dns.resolver
 
-#https://programmerclick.com/article/8131998611/
-
 class DNS:
    def __init__(self, dnsIPs):
       self.dnsIPs=dnsIPs #List of DNS ips     
-      #self.res = dns.resolver.Resolver(configure=False)
       self.res=dns.resolver.Resolver(configure=False)
-      #self.res.nameservers=self.dnsIPs
-
-   def consultDomain(self):
-      print(" DOMAIN = ", self.domain)
+      self.dicc={}
    
    def viewDNSIps(self):
       print(" MY DNS = ", self.dnsIPs)
 
-   def doConsult(self, domainToConsult):
+
+   def getIp(self, domainToConsult):
       """
       De dominio --> a IP
       """
+      self.dicc={} #Reset dicc
       for i in  self.dnsIPs:
         self.res.nameservers=[i]
         try:
           r=self.res.query(domainToConsult, 'a', tcp=True)
-          #print("RES-->",r)
           for rdata in r:
-            print( " --> DNS(",i,")server resolved IP:", rdata.to_text())
-          print("\n") #For view output better
+            if rdata.to_text() in self.dicc:
+              self.dicc[rdata.to_text()]=self.dicc[rdata.to_text()]+1
+            else:
+              self.dicc[rdata.to_text()]=1
         except:        
           print(" --> DNS(",i,") can not resolve this IP")
-         
 
-   def doConsult2(self, domainToConsult):
+      self.printDicc()
+      return self.dicc
+         
+   def getDomain(self, domainToConsult):
       """
       De dominio --> a servidores DNS
       """
       for i in  self.dnsIPs:
         try:
-          r=self.res.resolve(domainToConsult,'NS')
+          r=self.res.resolve(domainToConsult,'A')
           nameservers = [ns.to_text() for ns in r]
           print(nameservers)
         except:        
           print(" --> DNS(",i,") can not resolve this Domain")
+   
+   
+   def printDicc(self): 
+      """
+      Muestra nuestro diccionario
+      """
+     dictionary_items = self.dicc.items()
+     for item in dictionary_items:
+       print(item)
    
--- main.py
+++ main.py
@@ -1,29 +1,55 @@
 import os
+import sys # Para los argumentos
 from lib.ReadWriteFile.ReadFile import ReadFile
 from lib.DNS.DnsServer import DNS
 
 #GLOBAL VARS
 PATHdnsIPS= "/lib/DNS/dnsIp.txt" # All IPs from DNS
 DNSIP=[] # All IPs of DNS
+dns=None # Object DNS
 
 def loadDnsIP():
-    '''
-        We load all the IPs of DNS servers on DNSIP. The 
-        Ips are on the file "/lib/DNS/dnsIp.txt". 
-    '''
-    global DNSIP
-
-    rf= ReadFile(os.getcwd()+PATHdnsIPS) #Move to correct pwd
-    rf.openFile()
+  '''
+  We load all the IPs of DNS servers on DNSIP. The 
+  Ips are on the file "/lib/DNS/dnsIp.txt". 
+  '''
+  global DNSIP
+  rf= ReadFile(os.getcwd()+PATHdnsIPS) #Move to correct pwd
+  rf.openFile()
+  line=rf.readLine()
+  while line != "":
+    DNSIP.append(line)
     line=rf.readLine()
-    while line != "":
-        DNSIP.append(line)
-        line=rf.readLine()
+  rf.closeFile()
 
 
+def loadDns():
+  global dns
+  loadDnsIP()
+  dns=DNS(DNSIP)
+  
+def raiseError(s):
+  menu()
+  raise("Error: ", s)
+
+
+def menu():
+  None
+
+
+### MAIN ###
 if __name__ == "__main__":
-    loadDnsIP()
-    dns=DNS(DNSIP)
-    #dns.viewDNSIps()
-    #dns.doConsult("csp.omniaccess.com")
-    dns.doConsult2("google.com")
+  if len(sys.argv)>=2:
+    if sys.argv[1]== 'd':
+      print("DOMAIN ",sys.argv[2])
+      loadDns()
+      dns.getIp(sys.argv[2])
+    elif sys.argv[1]== 'i':
+      print("IP ",sys.argv[2])
+      loadDns()
+      dns.getDomain(sys.argv[2])
+    else:
+      raiseError("LOS PARAMETROS DE ENTRADA NO SON CORRECTOS")
+          
+  else:        
+    raiseError("Faltan argumentos")
